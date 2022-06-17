ee4b142aff6a8db73c135d44d6279d16d11313f0
Frank Wazeter <38684085+fwazeter@users.noreply.github.com>
2022-06-16 22:23:09 -0700

    Changes to Box block
    Previously, CSS custom props were re-declared in the class before their usage, however this overwrites inheritance. After testing, this is a behavior we want (inheritance), as opposed to theory where we thought that might interfere too much.
    
    For now ignore /image directory - it's test repository for fixing frame.

---
+++ image/save.js
@@ -0,0 +1,81 @@
+/**
+ * External dependencies
+ */
+import { isEmpty } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { RichText, useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * The save function defines the way in which the different attributes should
+ * be combined into the final markup, which is then serialized by the block
+ * editor into `post_content`.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#save
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function save( { attributes } ) {
+    const {
+        style,
+        mediaURL,
+        mediaAlt,
+        mediaCaption,
+        url,
+        rel,
+        linkClass,
+        linkTarget,
+        title,
+    } = attributes;
+
+    const ratioValue = style?.size.aspectRatio;
+    const portraitValue = style?.size.portraitRatio;
+
+    const styleProps = {}
+
+    if ( ratioValue !== '16 / 9' ) {
+        Object.assign( styleProps, { '--wf--ratio': ratioValue } );
+    }
+
+    if ( portraitValue !== '16 / 9' ) {
+        Object.assign( styleProps, { '--wf--portrait-ratio': portraitValue } );
+    }
+
+    const newRel = isEmpty( rel ) ? undefined : rel;
+
+    const image = (
+        <img
+            src={ mediaURL }
+            alt={ mediaAlt }
+            title={ title }
+        />
+    );
+
+    const figure = (
+        <>
+            { url ? (
+                <a
+                    className={ linkClass }
+                    href={ url }
+                    target={ linkTarget }
+                    rel={ newRel }
+                >
+                    { image }
+                </a>
+            ) : (
+                image
+            ) }
+            { ! RichText.isEmpty( mediaCaption ) && (
+                <RichText.Content tagName="figcaption" value={ mediaCaption } />
+            ) }
+        </>
+    );
+
+    return (
+        <figure { ...useBlockProps.save( { style: styleProps } ) }>
+            { figure }
+        </figure>
+    );
+}
